8b83921533a85052beffbaf9bd2995211894bb83
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-19 12:24:57 -0300

    Descrição do exercício 1.12.1

--- java-console/exercicio-1-12-1/src/Principal.java
+++ java-console/exercicio-1-12-1/src/Principal.java
@@ -1,3 +1,10 @@
+/*
+ 1.12.1. Fazer um algoritmo que:
+ Leia um número indeterminado de linhas contendo cada uma a idade de um indivíduo.
+ A última linha que não entrará nos cálculos, contém o valor da idade igual a zero.
+ Calcule e escreva a idade média deste grupo de indivíduos.
+ */
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
