6f05426b7d98d227e3349d4077c39d6c8e59a430
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-19 10:40:35 -0300

    Exercicio 1.12.1

---
+++ java-console/exercicio-1-12-1/src/Principal.java
@@ -0,0 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+
+  public static void main(String[] args) {
+    Scanner leitura = new Scanner(System.in);
+
+    double soma = 0.0;
+    int contadorDeIdade = 0;
+
+    try{
+      while (true) {
+        System.out.println("Digite uma idade");
+        int idade = leitura.nextInt();
+
+        if (idade == 0) {
+          break;
+        }
+        contadorDeIdade++;
+        soma += idade;
+      }
+    } catch (InputMismatchException e){
+      System.out.println("Digite um número inteiro para a idade");
+    }
+
+    if (contadorDeIdade > 0) {
+      System.out.println("Idade média: " + (soma / contadorDeIdade));
+    }
+
+  }
+
+}
